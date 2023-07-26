585cb75e1e7b5c894b6f92e3bc851f6cf6827b67
damien <damien911224@gmail.com>
2023-07-26 19:40:54 +0900

    Modified

--- libs/modeling/meta_archs.py
+++ libs/modeling/meta_archs.py
@@ -374,7 +374,7 @@ class TriDet(nn.Module):
         self.loss_normalizer_momentum = 0.9
 
         encoder_layer = DeformableTransformerEncoderLayer(fpn_dim, fpn_dim * 4, n_levels=len(self.fpn_strides))
-        self.deformable_encoder = DeformableTransformerEncoder(encoder_layer, 6)
+        self.deformable_encoder = DeformableTransformerEncoder(encoder_layer, 3)
 
         self.t_embeddings = nn.Embedding(3000, fpn_dim)
         self.level_embed = nn.Embedding(len(self.fpn_strides), fpn_dim)
