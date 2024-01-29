957fbba9f7f46f9114e0374ab8dcdeed0648a644
damien <damien911224@gmail.com>
2024-01-29 17:00:13 +0900

    Modified

--- pretrain_AF.py
+++ pretrain_AF.py
@@ -170,8 +170,8 @@ def main(args):
     # not ideal for multi GPU training, ok for now
     model = nn.DataParallel(model, device_ids=cfg['devices'])
 
-    checkpoint = torch.load(os.path.join(ckpt_root_folder, "pretrain/epoch_009.pth.tar"))
-    # checkpoint = torch.load(os.path.join("ckpt/kinetics_slowfast_AF_base_origin_filtered", "pretrain/epoch_010.pth.tar"))
+    # checkpoint = torch.load(os.path.join(ckpt_root_folder, "pretrain/epoch_009.pth.tar"))
+    checkpoint = torch.load(os.path.join("ckpt/kinetics_slowfast_TD_LTP_Re", "pretrain/epoch_014.pth.tar"))
     filtered_ckpt = dict()
     for k, v in checkpoint['state_dict_ema'].items():
         # if "query_embed" not in k:
